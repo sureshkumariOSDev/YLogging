51e9528e44bc1a3206e899c6e31301bf7bd6c50d
Suresh Kumar <suresh.r@niyati.com>
2018-06-18 15:02:40 +0530

    Intial commits

---
+++ YLogging/YLogging.h
@@ -0,0 +1,19 @@
+//
+//  YLogging.h
+//  YLogging
+//
+//  Created by MacMini on 18/06/18.
+//  Copyright © 2018 NIYATI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLogging.
+FOUNDATION_EXPORT double YLoggingVersionNumber;
+
+//! Project version string for YLogging.
+FOUNDATION_EXPORT const unsigned char YLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLogging/PublicHeader.h>
+
+
